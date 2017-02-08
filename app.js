f9430866c4276c9287cb1ec32a393876414be3f8
Jeremy Hancock <jeremyphancock@hotmail.com>
2017-02-08 15:12:19 +1100

    trying to get VS to work

---
+++ app.js
@@ -0,0 +1,49 @@
+function getAllBooks() {
+    var books = [
+        { title: 'Ulysses', author: 'James Joyce', available: false, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+    ];
+    return books;
+}
+function logFirstAvailable(books) {
+    var numberOfBooks = books.length;
+    var firstAvailable = '';
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var currentBook = books_1[_i];
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('Total books ' + numberOfBooks);
+    console.log('First available: ' + firstAvailable);
+}
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+})(Category || (Category = {}));
+function getBookTitlesByCategory(categoryFilter) {
+    console.log('Getting books in category : ' + Category[categoryFilter]);
+    var allBooks = getAllBooks();
+    var filteredBooks = [];
+    for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
+        var currentBook = allBooks_1[_i];
+        if (currentBook.category === categoryFilter) {
+            filteredBooks.push(currentBook.title);
+        }
+    }
+    return filteredBooks;
+}
+function logBookTitles(titles) {
+    for (var _i = 0, titles_1 = titles; _i < titles_1.length; _i++) {
+        var title = titles_1[_i];
+        console.log(title);
+    }
+}
+var poetryBooks = getBookTitlesByCategory(Category.Poetry);
+logBookTitles(poetryBooks);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,59 @@
+﻿
+
+function getAllBooks() {
+
+    let books = [
+        { title: 'Ulysses', author: 'James Joyce', available: false, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
+    ];
+
+    return books;
+}
+
+function logFirstAvailable(books): void {
+    let numberOfBooks: number = books.length;
+    let firstAvailable : string = '';
+
+    for (let currentBook of books) {
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+
+    console.log('Total books ' + numberOfBooks);
+    console.log('First available: ' + firstAvailable);
+}
+
+enum Category {
+    Biography,
+    Poetry,
+    Fiction
+}
+
+
+function getBookTitlesByCategory(categoryFilter: Category): Array<string> {
+    console.log('Getting books in category : ' + Category[categoryFilter]);
+    const allBooks = getAllBooks();
+    const filteredBooks: string[] = [];
+
+    for (let currentBook of allBooks) {
+        if (currentBook.category === categoryFilter) {
+            filteredBooks.push(currentBook.title);
+        }
+    }
+
+    return filteredBooks;
+}
+
+function logBookTitles(titles : string[]): void {
+    for (let title of titles) {
+        console.log(title);
+    }
+}
+
+const poetryBooks = getBookTitlesByCategory(Category.Poetry);
+
+logBookTitles(poetryBooks); 
